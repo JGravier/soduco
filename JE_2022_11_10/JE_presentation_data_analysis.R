ab1f5cd1fe078321c45875832b13349c386ec11e
Julie Gravier <JGravier@users.noreply.github.com>
2022-11-08 20:43:55 +0100

    update
    new case studies

--- JE_2022_11_10/JE_presentation_data_analysis.R
+++ JE_2022_11_10/JE_presentation_data_analysis.R
@@ -71,6 +71,42 @@ ggsave(filename = "outputs_visuals/street_network.png", plot = last_plot(),
 general_data <- read_excel(path = "geocoded_entries.xlsx")
 general_data
 
+# population data
+pop_before_1860 <- read_excel(path = "spatial_data/pop_avt_1860.xlsx")
+before_1860_paris <- pop_before_1860 %>%
+  summarise_if(is.numeric, sum, na.rm = TRUE)
+
+pop_after_1860 <- read_excel(path = "spatial_data/pop_apres_1860.xlsx")
+after_1860_paris <- pop_after_1860 %>%
+  summarise_if(is.numeric, sum, na.rm = TRUE)
+
+pop_all_long <- before_1860_paris %>%
+  select(`1836_S001_p8_pop_droit`:`1856_S001_p8_pop_droit`) %>%
+  pivot_longer(cols = everything(), names_to = "date", values_to = "pop") %>%
+  mutate(date = str_sub(string = date, start = 1, end = 4)) %>%
+  mutate(published = case_when(
+    date == 1841 ~ 1839,
+    date == 1846 ~ 1845,
+    date == 1856 ~ 1855,
+    TRUE ~ 0
+  )) %>%
+  bind_rows(
+    after_1860_paris %>%
+      select(`1866_S001_pop_droit`:`1896_S005_pop_fait`) %>%
+      pivot_longer(cols = everything(), names_to = "date", values_to = "pop") %>%
+      mutate(droifait = str_sub(string = date, start = 15, end = 20)) %>%
+      mutate(date = str_sub(string = date, start = 1, end = 4)) %>%
+      filter(droifait == "droit") %>%
+      select(-droifait) %>%
+      mutate(published = case_when(
+        date == 1866 ~ 1864,
+        date == 1876 ~ 1875,
+        date == 1886 ~ 1885,
+        date == 1891 ~ 1893,
+        TRUE ~ 0
+      ))
+  )
+
 #### adding informations from extracting NER elements patterns ####
 general_data_addin <- general_data %>%
   # when new line in string pattern "\n", function pattern does not work
@@ -110,6 +146,9 @@ max_freq <- function(x) {
 general_data_addin <- setDT(general_data_addin)[, freq_max:= max_freq(act_new), by = act_phoenetic][] %>%
   as_tibble()
 
+# specific for pres ~ no 1904
+general_data_addin <- general_data_addin %>%
+  filter(published != 1904)
 
 #### boulangers ####
 # exploration of patterns filtering on data
@@ -164,7 +203,7 @@ liste_patterns_profession <- boulanger_sf %>%
 
 # list of network equivalent length to matching choices with Annuaires
 list_of_networks <- list(reseau_jacoubet_1836, reseau_andriveau_1849, reseau_1854, reseau_1871, reseau_1871, reseau_poubelle_1888,
-                         reseau_poubelle_1888, reseau_poubelle_1888)
+                         reseau_poubelle_1888)
 
 # snaping points on networks
 liste_patterns_snap <- list()
@@ -194,9 +233,6 @@ netwokrs_carto_2 <- netwokrs_carto %>%
   )) %>%
   bind_rows(
     netwokrs_carto %>% filter(tempo %in% c(1871, 1888)) %>% mutate(source_annee = if_else(tempo == 1871, 1875, 1893))
-  ) %>%
-  bind_rows(
-    netwokrs_carto %>% filter(tempo == 1888) %>% mutate(source_annee = 1904)
   )
 
 ggplot() +
@@ -211,9 +247,320 @@ ggplot() +
         axis.title.y = element_blank()) +
   labs(subtitle = "Bakers",
        caption = "J. Gravier 2022 | ANR SoDUCo. Data: GeoHistoricalData") +
-  facet_wrap(~source_annee)
+  facet_wrap(~source_annee, nrow = 2)
 
 ggsave(filename = "outputs_visuals/bakers_cartography.png", plot = last_plot(), 
        dpi = 300, width = 25, height = 25, units = "cm")
+
+# writting for backgroundjob
+saveRDS(object = liste_patterns_snap, file = "data_for_bckgrd/bakers_list.rds")
+saveRDS(object = list_of_networks, file = "data_for_bckgrd/network_list.rds")
+
+
+### observed VS simulated data distances ####
+# reading results from background job: distances matrices
+tableau_plot <- readRDS("data_for_bckgrd/tableau_plot_bakers.rds")
+
+# plotting data 
+# !!! auto warning !!! breaks of axis x and y are here chosen
+plots_rss <- list()
+
+for (i in 1:length(tableau_plot)) {
+  plots_rss[[i]] <- tableau_plot[[i]] %>%
+    mutate(type = if_else(
+      condition = is.na(sim), "observed", "random\n(20 sim.)" 
+    )) %>%
+    ggplot(aes(x = dist_pi_p/1000, group = sim, color = type)) +
+    geom_density(size = 0.3) +
+    ggthemes::scale_color_colorblind() +
+    ggthemes::theme_igray() +
+    scale_x_continuous(name = "Distance (in km)", breaks = c(seq(0,15,2.5)), limits = c(0,14)) +
+    scale_y_continuous(breaks = c(seq(0, 0.35, 0.1)), limits = c(0,0.35)) +
+    ggtitle(tableau_plot[[i]]$source_annee[1])
+}
+
+distribution <- ggpubr::ggarrange(plotlist = plots_rss, 
+                                  common.legend = TRUE, legend = c("bottom"), nrow = 2, ncol = 4)
+
+ggpubr::annotate_figure(distribution, top = "Bakers", fig.lab.size = 15)
+
+ggsave(filename = "outputs_visuals/bakers_matrices_distribution.png", plot = last_plot(), 
+       dpi = 300, width = 25, height = 17, units = "cm")
+
+#### epiciers ####
+# exploration of patterns filtering on data
+general_data_addin %>%
+  mutate(patternsearch = str_detect(string = act_new, pattern = "epici|cpic|e picier")) %>%
+  filter(patternsearch == TRUE) %>%
+  select(activity, act_new, act_phoenetic) %>%
+  distinct() %>%
+  view()
+# need supressions too
+
+# creation of dataset specific to bakers
+epicier <- general_data_addin %>%
+  mutate(patternsearch = str_detect(string = act_new, pattern = "epici|cpic|e picier")) %>%
+  filter(patternsearch == TRUE) %>%
+  mutate(patternsearch = str_detect(string = act_new, pattern = "repic|qepic")) %>%
+  filter(patternsearch == FALSE)
+
+#### carto initiale ####
+# create boulanger spatial data as "certain" (need improvment! here just nb_loc = 1)
+# critical vision
+epicier %>%
+  mutate(geoloca = if_else(condition = is.na(precise.geom), "no", "yes")) %>%
+  group_by(nb_loc, geoloca) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  mutate(freq = n/sum(n)) %>%
+  ggplot(aes(x = nb_loc, y = freq, fill = geoloca)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  ggthemes::scale_fill_economist() +
+  scale_x_continuous(name = "n locations by entry", breaks = seq(1, 12, 1)) +
+  labs(subtitle = "Grocers in selected Annuaires",
+       caption = "J. Gravier 2022 | ANR SoDUCo. Data: Annuaires, GeoHistoricalData")
+
+ggsave(filename = "outputs_visuals/epiciers_sources_criticism.png", plot = last_plot(), 
+       dpi = 300, width = 16, height = 11, units = "cm")
+
+# dataset
+epicier_sf <- epicier %>%
+  mutate(geoloca = if_else(condition = is.na(precise.geom), "no", "yes")) %>%
+  filter(geoloca == "yes") %>%
+  filter(nb_loc == 1 & !is.na(overall_score)) %>%
+  st_as_sf(wkt = "precise.geom", crs = 2154)
+
+tm_shape(epicier_sf) + tm_dots()
+
+### snap points patterns of Annuaires on each of matching network
+# list of snap_points by Annuaire
+liste_patterns_profession <- epicier_sf %>%
+  arrange(published) %>%
+  group_split(.tbl = ., published) # equivalent to date
+
+# snaping points on networks
+liste_patterns_snap <- list()
+
+for (i in 1:length(liste_patterns_profession)) {
+  snap_point_geometrique <- maptools::snapPointsToLines(points = as(liste_patterns_profession[[i]], "Spatial"), 
+                                                        lines = as(list_of_networks[[i]], "Spatial"), 
+                                                        maxDist = 100, idField = "rowid")
+  
+  snap_point_geometrique <- st_as_sf(snap_point_geometrique) %>%
+    mutate(source = liste_patterns_profession[[i]]$directory[1]) %>%
+    mutate(source_annee = liste_patterns_profession[[i]]$published[1])
+  
+  liste_patterns_snap[[i]] <- snap_point_geometrique
+  
+}
+
+## cartography of epicier_sf
+liste_snap_as_one_sf <- do.call(rbind, liste_patterns_snap)
+
+ggplot() +
+  geom_sf(data = netwokrs_carto_2, color = "grey70", size = 0.2) +
+  geom_sf(data = liste_snap_as_one_sf, alpha = 0.7, color = "red") +
+  ggspatial::annotation_scale(location = "tr",  width_hint = 0.2) +
+  ggthemes::theme_igray() +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  labs(subtitle = "Grocers - 'épiciers'",
+       caption = "J. Gravier 2022 | ANR SoDUCo. Data: GeoHistoricalData") +
+  facet_wrap(~source_annee, nrow = 2)
+
+ggsave(filename = "outputs_visuals/grocers_cartography.png", plot = last_plot(), 
+       dpi = 300, width = 25, height = 25, units = "cm")
+
+# writting for backgroundjob
+saveRDS(object = liste_patterns_snap, file = "data_for_bckgrd/grocers_list.rds")
+
+### observed VS simulated data distances ####
+# reading results from background job: distances matrices
+tableau_plot <- readRDS("data_for_bckgrd/tableau_plot_grocers.rds")
+
+# plotting data
+# !!! auto warning !!! breaks of axis x and y are here chosen
+plots_rss <- list()
+
+for (i in 1:length(tableau_plot)) {
+  plots_rss[[i]] <- tableau_plot[[i]] %>%
+    mutate(type = if_else(
+      condition = is.na(sim), "observed", "random\n(20 sim.)" 
+    )) %>%
+    ggplot(aes(x = dist_pi_p/1000, group = sim, color = type)) +
+    geom_density(size = 0.3) +
+    ggthemes::scale_color_colorblind() +
+    ggthemes::theme_igray() +
+    scale_x_continuous(name = "Distance (in km)", breaks = c(seq(0,15,2.5)), limits = c(0,14)) +
+    scale_y_continuous(breaks = c(seq(0, 0.35, 0.1)), limits = c(0,0.35)) +
+    theme(axis.title = element_text(size = 10),
+          axis.text = element_text(size = 8)) +
+    ggtitle(tableau_plot[[i]]$source_annee[1])
+}
+
+distribution <- ggpubr::ggarrange(plotlist = plots_rss, 
+                                  common.legend = TRUE, legend = c("bottom"), nrow = 2, ncol = 4)
+
+ggpubr::annotate_figure(distribution, top = "Grocers - 'épiciers'", fig.lab.size = 15)
+
+ggsave(filename = "outputs_visuals/grocers_matrices_distribution.png", plot = last_plot(), 
+       dpi = 300, width = 25, height = 17, units = "cm")
+
+
+#### bijoutiers ####
+# creation of dataset specific to bakers
+bijoutier <- general_data_addin %>%
+  mutate(pattern = str_detect(string = act_new, pattern = "bijo")) %>% 
+  filter(pattern == TRUE) %>%
+  mutate(bijoutiers_distinction = case_when(
+    str_detect(act_phoenetic, pattern = "B") ~ "Bijoutier",
+    str_detect(act_phoenetic, pattern = "J") ~ "Joalliers-bijoutier",
+    str_detect(act_new, pattern = "fabr.|fab") ~ "Fabricant de bijoux",
+    TRUE ~ "Autres"
+  )) %>%
+  filter(bijoutiers_distinction != "Autres") %>%
+  select(-bijoutiers_distinction, -pattern)
+
+bijoutier %>%
+  select(activity, act_new, act_phoenetic) %>%
+  distinct() %>%
+  view()
+
+#### carto initiale ####
+# create boulanger spatial data as "certain" (need improvment! here just nb_loc = 1)
+# critical vision
+bijoutier %>%
+  mutate(geoloca = if_else(condition = is.na(precise.geom), "no", "yes")) %>%
+  group_by(nb_loc, geoloca) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  mutate(freq = n/sum(n)) %>%
+  ggplot(aes(x = nb_loc, y = freq, fill = geoloca)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  ggthemes::scale_fill_economist() +
+  scale_x_continuous(name = "n locations by entry", breaks = seq(1, 12, 1)) +
+  labs(subtitle = "Jewellers in selected Annuaires",
+       caption = "J. Gravier 2022 | ANR SoDUCo. Data: Annuaires, GeoHistoricalData")
+
+ggsave(filename = "outputs_visuals/jewellers_sources_criticism.png", plot = last_plot(), 
+       dpi = 300, width = 16, height = 11, units = "cm")
+
+# dataset
+bijoutier_sf <- bijoutier %>%
+  mutate(geoloca = if_else(condition = is.na(precise.geom), "no", "yes")) %>%
+  filter(geoloca == "yes") %>%
+  filter(nb_loc == 1 & !is.na(overall_score)) %>%
+  st_as_sf(wkt = "precise.geom", crs = 2154)
+
+tm_shape(bijoutier_sf) + tm_dots()
+
+### snap points patterns of Annuaires on each of matching network
+# list of snap_points by Annuaire
+liste_patterns_profession <- bijoutier_sf %>%
+  arrange(published) %>%
+  group_split(.tbl = ., published) # equivalent to date
+
+# snaping points on networks
+liste_patterns_snap <- list()
+
+for (i in 1:length(liste_patterns_profession)) {
+  snap_point_geometrique <- maptools::snapPointsToLines(points = as(liste_patterns_profession[[i]], "Spatial"), 
+                                                        lines = as(list_of_networks[[i]], "Spatial"), 
+                                                        maxDist = 100, idField = "rowid")
+  
+  snap_point_geometrique <- st_as_sf(snap_point_geometrique) %>%
+    mutate(source = liste_patterns_profession[[i]]$directory[1]) %>%
+    mutate(source_annee = liste_patterns_profession[[i]]$published[1])
+  
+  liste_patterns_snap[[i]] <- snap_point_geometrique
+  
+}
+
+## cartography of epicier_sf
+liste_snap_as_one_sf <- do.call(rbind, liste_patterns_snap)
+
+ggplot() +
+  geom_sf(data = netwokrs_carto_2, color = "grey70", size = 0.2) +
+  geom_sf(data = liste_snap_as_one_sf, alpha = 0.7, color = "red") +
+  ggspatial::annotation_scale(location = "tr",  width_hint = 0.2) +
+  ggthemes::theme_igray() +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  labs(subtitle = "Jewellers - 'bijoutiers'",
+       caption = "J. Gravier 2022 | ANR SoDUCo. Data: GeoHistoricalData") +
+  facet_wrap(~source_annee, nrow = 2)
+
+ggsave(filename = "outputs_visuals/jewellers_cartography.png", plot = last_plot(), 
+       dpi = 300, width = 25, height = 25, units = "cm")
+
+# writting for backgroundjob
+saveRDS(object = liste_patterns_snap, file = "data_for_bckgrd/jewellers_list.rds")
+
+### observed VS simulated data distances ####
+# reading results from background job: distances matrices
+tableau_plot <- readRDS("data_for_bckgrd/tableau_plot_grocers.rds")
+
+# plotting data
+# !!! auto warning !!! breaks of axis x and y are here chosen
+plots_rss <- list()
+
+for (i in 1:length(tableau_plot)) {
+  plots_rss[[i]] <- tableau_plot[[i]] %>%
+    mutate(type = if_else(
+      condition = is.na(sim), "observed", "random\n(20 sim.)" 
+    )) %>%
+    ggplot(aes(x = dist_pi_p/1000, group = sim, color = type)) +
+    geom_density(size = 0.3) +
+    ggthemes::scale_color_colorblind() +
+    ggthemes::theme_igray() +
+    scale_x_continuous(name = "Distance (in km)", breaks = c(seq(0,15,2.5)), limits = c(0,14)) +
+    scale_y_continuous(breaks = c(seq(0, 0.35, 0.1)), limits = c(0,0.35)) +
+    theme(axis.title = element_text(size = 10),
+          axis.text = element_text(size = 8)) +
+    ggtitle(tableau_plot[[i]]$source_annee[1])
+}
+
+distribution <- ggpubr::ggarrange(plotlist = plots_rss, 
+                                  common.legend = TRUE, legend = c("bottom"), nrow = 2, ncol = 4)
+
+ggpubr::annotate_figure(distribution, top = "Grocers - 'épiciers'", fig.lab.size = 15)
+
+ggsave(filename = "outputs_visuals/grocers_matrices_distribution.png", plot = last_plot(), 
+       dpi = 300, width = 25, height = 17, units = "cm")
+
+
+#### temporal analysis with population ####
+epicier %>%
+  group_by(published) %>%
+  summarise(n = n()) %>%
+  left_join(x = ., y = pop_all_long, by = "published") %>%
+  mutate(n_pop = n/pop*10000) %>%
+  ggplot(aes(x = published, y = n_pop)) +
+  geom_line() +
+  geom_point() +
+  ggthemes::scale_fill_economist() +
+  theme(axis.title = element_blank()) +
+  labs(subtitle = "N grocers per 10,000 inhabitants - 'épiciers'",
+       caption = "J. Gravier 2022 | ANR SoDUCo. Data: Annuaires, GeoHistoricalData")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
