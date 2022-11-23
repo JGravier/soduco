0e2ef079cb604ee76863e2a69cb133fd9df13ebb
Julie Gravier <JGravier@users.noreply.github.com>
2022-11-23 13:06:43 +0100

    update documentation

--- ranking/rank_turnover_ot.R
+++ ranking/rank_turnover_ot.R
@@ -5,6 +5,7 @@ library(data.table)
 #### tradeve database ####
 tradeve <- read_excel(path = "ranking_exploration/TRADEVE_database/TRADEVE_UrbanAreas_Data.xlsx", sheet = "UrbanAreas_Data")
 
+# creation of ranking by countries
 dk_countries_tradeve <- tradeve %>%
   group_by(Country) %>%
   mutate(rang_1961 = rank(desc(Pop_1961)),
@@ -14,15 +15,23 @@ dk_countries_tradeve <- tradeve %>%
          rang_2001 = rank(desc(Pop_2001)),
          rang_2011 = rank(desc(Pop_2011)))
 
+
 #### rank turnover calculations ####
+# as defined in Iniguez et al. 2022
+# G. Iñiguez, C. Pineda, C. Gershenson, et A.-L. Barabási, 
+# « Dynamics of ranking », Nat Commun, vol. 13, nᵒ 1, Art. nᵒ 1, 2022, doi: 10.1038/s41467-022-29256-x
+
+# list of countries studied
 liste_pays <- c("DE", "CZ", "ES", "FR", "UK", "IT", "NL", "PL", "RO")
-
+# test of diverse ranking sizes
 N0 <- seq(2, 768, 5)
-
+# output tibble of rank turnover
 tableau_turnover <- tibble(Country = character(), time_t = numeric(), Nt = numeric(), N0 = numeric())
-  
+
+# loop to create rank turnover by countries, considering diverse ranking sizes
 for (k in 1:length(liste_pays)) {
   
+  # filtering init tibble by country and creation of list of tibbles by periods t analysed
   tableau <- dk_countries_tradeve %>%
     filter(Country == liste_pays[k]) %>%
     rowid_to_column() %>%
@@ -35,6 +44,7 @@ for (k in 1:length(liste_pays)) {
     
     liste_filtre_N0 <- list()
     
+    # filtering data by ranking size for a time t
     for (j in 1:length(tableau)) {
       
       liste_filtre_N0[[j]] <- tableau[[j]] %>%
@@ -43,13 +53,13 @@ for (k in 1:length(liste_pays)) {
       
     }
     
-    tableau_filtre_N0 <- data.table::rbindlist(l = liste_filtre_N0) %>%
-      as_tibble()
+    tableau_filtre_N0 <- data.table::rbindlist(l = liste_filtre_N0) %>% # binding all tibbles in list in one tibble
+      as_tibble() 
     
     tabletableau_Nt_N0 <- tibble(Country = character(), time_t = numeric(), Nt = numeric(), N0 = numeric())
     
     for (j in 1:(length(liste_filtre_N0))) {
-      
+      # calculate rank turnover for each time t periods
       tableau_intermediaire <- tableau_filtre_N0 %>%
         filter(time_t <= j) %>%
         bind_rows(
@@ -65,7 +75,7 @@ for (k in 1:length(liste_pays)) {
         bind_rows(tableau_intermediaire)
     }
     
-    print(i)
+    print(k)
     print(N0[i])
     
     tableau_turnover <- tableau_turnover %>%
@@ -75,15 +85,17 @@ for (k in 1:length(liste_pays)) {
   
 }
 
+# size of each country system
 no_max_countries <- dk_countries_tradeve %>%
   filter(Country %in% c("DE", "CZ", "ES", "FR", "UK", "IT", "NL", "PL", "RO"))  %>%
   summarise(N = n())
 
+# filtering rank turnover considering the size N of country system
 tableau_turnover <- tableau_turnover %>%
   left_join(x = ., y = no_max_countries, by = "Country") %>%
   filter(N0 <= N)
 
-
+# a visualisation
 tableau_turnover %>%
   filter(N0 == 52) %>%
   mutate(Ot = Nt/N0) %>%
