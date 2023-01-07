60a9fb725c704e2d805dacb99f39c450a2785cae
Julie Gravier <JGravier@users.noreply.github.com>
2023-01-07 14:12:33 +0100

    update
    need > analyzing diff/jump model with diverse probability of permutation

--- ranking/ranking_full_random.R
+++ ranking/ranking_full_random.R
@@ -1,13 +1,17 @@
 library(tidyverse)
+library(latex2exp)
+library(data.table)
+source(file = "permutation_function.R")
 
-#### random list replacement with system of size 1000 on 10 time t ####
+#### Full random ####
+#### random list replacement with system of size 1000 on 10 time t 
 
 N0 <- seq(1, 1000, 1)
-n_sampling <- 10
+t <- 10
 
 tibble_output <- tibble(value = numeric(), sim = numeric())
 
-for (j in 1:n_sampling) {
+for (j in 1:t) {
     
     tableau_init <- sample(x = N0, size = length(N0), replace = FALSE) %>% 
       as_tibble() %>%
@@ -20,7 +24,7 @@ for (j in 1:n_sampling) {
 }
 
 
-#### rank turnover Ot ####
+#### rank turnover Ot
 tibble_output_rowid <- tibble_output %>%
   group_by(sim) %>%
   mutate(rowid = row_number())
@@ -29,7 +33,6 @@ tibble_output_rowid <- tibble_output %>%
 tableau_turnover <- tibble(time_t = numeric(), Nt = numeric(), N0 = numeric())
 
 # loop to create rank turnover considering diverse ranking sizes
-# filtering init tibble by country and creation of list of tibbles by periods t analysed
 tableau <- tibble_output_rowid %>%
   ungroup() %>%
   group_split(sim)
@@ -76,11 +79,12 @@ for (i in 2:length(x = N0)) {
 }
 
 # output data calculations
-write.csv(x = tableau_turnover, file = "ranking_exploration/outputs_data/o_point_random_size1000_t10.csv", 
+write.csv(x = tableau_turnover, file = "outputs_data/o_point_random_size1000_t10.csv", 
           row.names = FALSE)
 
+tableau_turnover <- read.csv(file = "outputs_data/o_point_random_size1000_t10.csv")
 
-#### plotting diverse Ot on N0 ####
+#### plotting diverse Ot on N0 
 tableau_turnover %>%
   mutate(Ot = Nt/N0) %>%
   filter(N0 %in% seq(2, 1002, 50)) %>%
@@ -89,14 +93,14 @@ tableau_turnover %>%
   geom_line() +
   theme_bw() +
   scale_x_continuous(name = TeX(r"($t$)"), breaks = seq(1,10,1)) +
-  ylab(TeX(r"($O_{t}$)")) +
+  scale_y_continuous(name = TeX(r"($O_{t}$)"), breaks = seq(1,10,1)) +
   labs(caption = "J. Gravier, 2022", 
        title = "Turnover rank: random situation", subtitle = "N=1000, t=10")
 
 ggsave(filename = "ranking_exploration/rank_turnover_ot_random.png", plot = last_plot(), 
        width = 18, height = 12, units = 'cm')
 
-#### plotting dot O ####
+#### plotting dot O 
 tableau_turnover %>%
   mutate(Ot = Nt/N0) %>%
   filter(time_t %in% c(1, 10)) %>%
@@ -111,5 +115,246 @@ tableau_turnover %>%
   labs(caption = "J. Gravier, 2022", 
        title = "Mean turnover rank rate: random situation", subtitle = "N=1000, t=10")
 
-ggsave(filename = "ranking_exploration/rank_turnover_rate_normalized_random.png", plot = last_plot(), 
+ggsave(filename = "rank_turnover_rate_normalized_random.png", plot = last_plot(), 
        width = 18, height = 12, units = 'cm')
+
+
+
+#### Other models ####
+# with N0 is constant over time
+# with diffusion a proba alpha
+# jump = 1 - alpha
+# permutation t-1, t = a(1-a)
+diff_proba <- seq(0.01, 0.99, 0.01)
+jump_proba <- 1-diff_proba
+perm_proba <- diff_proba*jump_proba
+max(perm_proba)
+
+#### system of size 1000 on 10 times
+N0 <- seq(1, 1000, 1)
+t <- seq(1,10,1)
+
+#### only jump model ####
+# including path dependecy of the system
+permutation_proba <- list()
+permutation_proba[[1]] <- N0
+
+### for t times periods
+for (t in 2:length(t)) {
+  
+  permutation_proba[[t]] <- jump_permutation(vector = permutation_proba[[t-1]], prob = 0.25)
+  
+}
+
+df_permutation_proba <- tibble(value = numeric(), time = numeric())
+
+for (i in 1:length(permutation_proba)) {
+  
+  df_permutation_proba <- df_permutation_proba %>%
+    bind_rows(permutation_proba[[i]] %>%
+                as_tibble() %>%
+                mutate(time = i))
+
+  }
+
+df_permutation_proba
+
+#### analysis of time model
+#### rank turnover Ot
+tibble_output_rowid <- df_permutation_proba %>%
+  group_by(time) %>%
+  mutate(rowid = row_number())
+
+# output tibble of rank turnover
+tableau_turnover <- tibble(time_t = numeric(), Nt = numeric(), N0 = numeric())
+
+# loop to create rank turnover considering diverse ranking sizes
+tableau <- tibble_output_rowid %>%
+  ungroup() %>%
+  group_split(time)
+
+for (i in 2:length(x = N0)) {
+  
+  liste_filtre_N0 <- list()
+  
+  # filtering data by ranking size for a time t
+  for (j in 1:length(tableau)) {
+    
+    liste_filtre_N0[[j]] <- tableau[[j]] %>%
+      filter(value <= N0[i]) %>%
+      mutate(time_t = j)
+    
+  }
+  
+  tableau_filtre_N0 <- data.table::rbindlist(l = liste_filtre_N0) %>% # binding all tibbles in list in one tibble
+    as_tibble()
+  
+  tabletableau_Nt_N0 <- tibble(time_t = numeric(), Nt = numeric(), N0 = numeric())
+  
+  for (j in 1:length(liste_filtre_N0)) {
+    # calculate rank turnover for each time t periods
+    tableau_intermediaire <- tableau_filtre_N0 %>%
+      filter(time_t <= j) %>%
+      bind_rows(
+        tableau_filtre_N0 %>%
+          filter(time_t == j)
+      ) %>%
+      summarise(Nt = n_distinct(rowid)) %>%
+      mutate(time_t = j,
+             N0 = N0[i])
+    
+    tabletableau_Nt_N0 <- tabletableau_Nt_N0 %>%
+      bind_rows(tableau_intermediaire)
+  }
+  
+  print(i)
+  
+  tableau_turnover <- tableau_turnover %>%
+    bind_rows(tabletableau_Nt_N0)
+  
+}
+
+tableau_turnover
+
+# output data calculations
+write.csv(x = tableau_turnover, file = "o_t_jumpmodel_size1000_t10.csv", 
+          row.names = FALSE)
+
+
+#### jump and diffusion model ####
+# including path dependecy of the system
+N0 <- seq(1, 1000, 1)
+t <- seq(1,10,1)
+
+permutation_proba <- list()
+permutation_proba[[1]] <- N0
+
+### for t times periods
+for (t in 2:length(t)) {
+  
+  permutation_proba[[t]] <- jump_diff_permutation(vector = permutation_proba[[t-1]], 
+                                                  prob = 0.25*length(permutation_proba[[t-1]]))
+  
+}
+
+df_permutation_proba <- tibble(value = numeric(), time = numeric())
+
+for (i in 1:length(permutation_proba)) {
+  
+  df_permutation_proba <- df_permutation_proba %>%
+    bind_rows(permutation_proba[[i]] %>%
+                as_tibble() %>%
+                mutate(time = i))
+  
+}
+
+df_permutation_proba
+
+#### analysis of time model
+#### rank turnover Ot
+tibble_output_rowid <- df_permutation_proba %>%
+  group_by(time) %>%
+  mutate(rowid = row_number())
+
+# output tibble of rank turnover
+tableau_turnover <- tibble(time_t = numeric(), Nt = numeric(), N0 = numeric())
+
+# loop to create rank turnover considering diverse ranking sizes
+tableau <- tibble_output_rowid %>%
+  ungroup() %>%
+  group_split(time)
+
+for (i in 2:length(x = N0)) {
+  
+  liste_filtre_N0 <- list()
+  
+  # filtering data by ranking size for a time t
+  for (j in 1:length(tableau)) {
+    
+    liste_filtre_N0[[j]] <- tableau[[j]] %>%
+      filter(value <= N0[i]) %>%
+      mutate(time_t = j)
+    
+  }
+  
+  tableau_filtre_N0 <- data.table::rbindlist(l = liste_filtre_N0) %>% # binding all tibbles in list in one tibble
+    as_tibble()
+  
+  tabletableau_Nt_N0 <- tibble(time_t = numeric(), Nt = numeric(), N0 = numeric())
+  
+  for (j in 1:length(liste_filtre_N0)) {
+    # calculate rank turnover for each time t periods
+    tableau_intermediaire <- tableau_filtre_N0 %>%
+      filter(time_t <= j) %>%
+      bind_rows(
+        tableau_filtre_N0 %>%
+          filter(time_t == j)
+      ) %>%
+      summarise(Nt = n_distinct(rowid)) %>%
+      mutate(time_t = j,
+             N0 = N0[i])
+    
+    tabletableau_Nt_N0 <- tabletableau_Nt_N0 %>%
+      bind_rows(tableau_intermediaire)
+  }
+  
+  print(i)
+  
+  tableau_turnover <- tableau_turnover %>%
+    bind_rows(tabletableau_Nt_N0)
+  
+}
+
+tableau_turnover
+
+write.csv(x = tableau_turnover, file = "o_t_jumpdiffmodel_size1000_t10.csv", 
+          row.names = FALSE)
+
+#### plotting diverse models ####
+tableau_turnover <- read.csv(file = "o_t_jumpmodel_size1000_t10.csv") %>%
+  as_tibble()
+
+tableau_turnover_m1 <- read.csv(file = "o_t_jumpdiffmodel_size1000_t10.csv") %>%
+  as_tibble()
+
+tableau_turnover <- tableau_turnover %>%
+  mutate(model = "Permutation only") %>%
+  bind_rows(tableau_turnover_m1 %>% 
+              mutate(model = "Permutation & diffusion"))
+
+#### plotting diverse Ot on N0 
+tableau_turnover %>%
+  mutate(Ot = Nt/N0) %>%
+  # filter(N0 %in% seq(2, 1002, 25)) %>%
+  ggplot(aes(x = time_t, y = Ot, color = N0, group = N0)) +
+  scale_color_viridis_c(option = "C") +
+  geom_line() +
+  theme_bw() +
+  scale_x_continuous(name = TeX(r"($t$)"), breaks = seq(1,10,1)) +
+  scale_y_continuous(name = TeX(r"($O_{t}$)"), breaks = seq(1,10,1)) +
+  labs(caption = "J. Gravier, 2022", 
+       title = "Rank turnover", subtitle = "N=1000, t=10") +
+  facet_wrap(~ model)
+
+ggsave(filename = "rank_turnover_models.png", plot = last_plot(), 
+       width = 18, height = 12, units = 'cm')
+
+#### plotting dot O 
+tableau_turnover %>%
+  mutate(Ot = Nt/N0) %>%
+  filter(time_t %in% c(1, 10)) %>%
+  mutate(time_t = paste0("t_", time_t)) %>%
+  pivot_wider(id_cols = c(N0, model), names_from = time_t, values_from = Ot) %>%
+  mutate(o_point = (t_10 - t_1)/10) %>%
+  ggplot(aes(x = N0/1000, y = o_point)) +
+  geom_line() +
+  theme_bw() +
+  scale_x_continuous(name = TeX(r"($N_{0}/N$)")) +
+  ylab(TeX(r"($\dot{O}$)")) +
+  labs(caption = "J. Gravier, 2022", 
+       title = "Mean turnover rank rate", subtitle = "N=1000, t=10") +
+  facet_wrap(~ model)
+
+ggsave(filename = "rank_turnover_rate_normalized_models.png", plot = last_plot(), 
+       width = 18, height = 12, units = 'cm')
+
