f0c5946e4edd9d00fd59f14299a73b590b575c92
Julie Gravier <JGravier@users.noreply.github.com>
2023-01-27 09:47:35 +0100

    update for pers archive

--- ranking/ranking_permutation_model.R
+++ ranking/ranking_permutation_model.R
@@ -5,6 +5,189 @@ source(file = "permutation_function.R")
 
 set.seed(2123)
 
+#### Jump and replacement model ####
+# with N0 is constant over time
+# perm prob is jump proba here
+perm_proba <- seq(0.05, 1, 0.05)
+
+#### system of size 1000
+N0 <- seq(1, 1000, 1)
+timest <- seq(1,5,1)
+
+
+model_list <- list()
+
+for (p in 1:length(perm_proba)) {
+  # create a list of rank
+  permutation_proba <- list()
+  permutation_proba[[1]] <- N0
+  
+  ### for t times periods
+  # including path dependency of the system
+  for (t in 2:length(timest)) {
+    permutation_proba[[t]] <- jump_replacement_not_full_replace(vector = permutation_proba[[t-1]], prob = perm_proba[p])
+  }
+  
+  model_list[[p]] <- unlist(permutation_proba) %>%
+    as_tibble() %>%
+    rename(rank = value) %>%
+    mutate(proba = perm_proba[p]) %>%
+    mutate(timest = rep(x = timest, each = length(N0)))
+}
+
+
+### Rank turnover cals: Ot ####
+# loop to create rank turnover considering diverse ranking sizes (<=> N0)
+
+model_list_turnover <- list()
+
+for (l in 1:length(model_list)) {
+  
+  tableau <- model_list[[l]] %>%
+    group_by(timest) %>%
+    mutate(rowid = row_number()) %>%
+    ungroup() %>%
+    group_split(timest)
+  
+  # output tibble of rank turnover
+  tableau_turnover <- tibble(time_t = numeric(), Nt = numeric(), N0 = numeric(), proba = numeric())
+  
+  for (i in 2:length(x = N0)) {
+    
+    liste_filtre_N0 <- list()
+    
+    # filtering data by ranking size for a time t
+    for (j in 1:length(tableau)) {
+      
+      liste_filtre_N0[[j]] <- tableau[[j]] %>%
+        filter(rank <= N0[i]) %>%
+        mutate(time_t = j)
+      
+    }
+    
+    tableau_filtre_N0 <- data.table::rbindlist(l = liste_filtre_N0) %>% # binding all tibbles in list in one tibble
+      as_tibble()
+    
+    tabletableau_Nt_N0 <- tibble(time_t = numeric(), Nt = numeric(), N0 = numeric(), proba = numeric())
+    
+    for (j in 1:length(liste_filtre_N0)) {
+      # calculate rank turnover for each time t periods
+      tableau_intermediaire <- tableau_filtre_N0 %>%
+        filter(time_t <= j) %>%
+        bind_rows(
+          tableau_filtre_N0 %>%
+            filter(time_t == j)
+        ) %>%
+        summarise(Nt = n_distinct(rowid)) %>%
+        mutate(time_t = j,
+               N0 = N0[i])
+      
+      tabletableau_Nt_N0 <- tabletableau_Nt_N0 %>%
+        bind_rows(tableau_intermediaire)
+    }
+    
+    print(l)
+    print(i)
+    
+    tableau_turnover <- tableau_turnover %>%
+      bind_rows(tabletableau_Nt_N0) %>%
+      mutate(proba = model_list[[l]]$proba[1])
+    
+  }
+  
+  model_list_turnover[[l]] <- tableau_turnover
+  
+}
+
+model_output <- rbindlist(model_list_turnover) %>%
+  as_tibble(x = .) 
+
+# model output
+write.csv(x = model_output,
+          file = "outputs_data/models/o_t_jumpmodel_equals_size1000_t10_p0_to_0_5.csv", 
+          row.names = FALSE)
+
+
+### Rank flux cals: F ####
+# output tibble of rank flux
+tableau_flux <- tibble(Ft = numeric(), proba = numeric(), time_t = numeric(), time_t1 = numeric(), N0 = numeric())
+
+# loop to create rank flux considering diverse ranking sizes (<=> N0)
+for (l in 1:length(model_list)) {
+  
+  tableau <- model_list[[l]] %>%
+    group_by(timest) %>%
+    mutate(rowid = row_number()) %>%
+    ungroup() %>%
+    group_split(timest)
+  
+  for (i in 1:length(x = N0)) {
+    
+    liste_filtre_N0 <- list()
+    
+    # filtering data by ranking size for a time t
+    for (j in 1:length(tableau)) {
+      
+      liste_filtre_N0[[j]] <- tableau[[j]] %>%
+        filter(rank <= N0[i]) %>%
+        mutate(time_t = j)
+      
+    }
+    
+    tableau_filtre_N0 <- data.table::rbindlist(l = liste_filtre_N0) %>% # binding all tibbles in list in one tibble
+      as_tibble()
+    
+    tabletableau_Ft_N0 <- tibble(Ft = numeric(), proba = numeric(), time_t = numeric(), time_t1 = numeric(), N0 = numeric())
+    
+    for (j in 1:(length(liste_filtre_N0)-1)) {
+      # calculate rank turnover for each time t periods
+      tableau_intermediaire <- tableau_filtre_N0 %>%
+        filter(time_t == j) %>%
+        bind_rows(
+          tableau_filtre_N0 %>%
+            filter(time_t == j + 1)
+        ) %>%
+        select(rowid) %>%
+        unique() %>% 
+        count() %>%
+        rename(Ft = n) %>%
+        mutate(proba = tableau_filtre_N0$proba[1],
+               time_t = j,
+               time_t1 = j+1,
+               N0 = N0[i])
+      
+      tabletableau_Ft_N0 <- tabletableau_Ft_N0 %>%
+        bind_rows(tableau_intermediaire)
+    }
+    
+    print(l)
+    print(N0[i])
+    
+    tableau_flux <- tableau_flux %>%
+      bind_rows(tabletableau_Ft_N0)
+    
+  }
+  
+}
+
+
+Ft <- tableau_flux %>%
+  mutate(Ft_proba = (Ft-N0)/N0) %>%
+  group_by(proba, N0) %>%
+  summarise(Ft_proba = mean(Ft_proba)) 
+
+
+# model output
+write.csv(x = tableau_flux, 
+          file = "outputs_data/f_t_jumpdiffmodel_size1000_t10_p20.csv",
+          row.names = FALSE)
+
+write.csv(x = Ft, 
+          file = "outputs_data/Flux_mean_t_jumpdiffmodel_size1000_t10_p20.csv", 
+          row.names = FALSE)
+
+
+#### jump and diff model ####
 # with N0 is constant over time
 # with diffusion a proba alpha
 # jump = 1 - alpha
@@ -18,7 +201,7 @@ perm_proba <- seq(0.001, 0.25, 0.0249)
 N0 <- seq(1, 1000, 1)
 timest <- seq(1,10,1)
 
-#### only jump and diff model ####
+
 model_list <- list()
 
 for (p in 1:length(perm_proba)) {
@@ -192,23 +375,6 @@ write.csv(x = Ft,
 
 
 #### visual analytics ####
-diff_proba <- seq(0.01, 0.99, 0.05)
-jump_proba <- 1-diff_proba
-perm_proba <- diff_proba*jump_proba
-
-visuproba <- tibble(diffusion=diff_proba, jump=jump_proba, permutation=perm_proba)
-
-visuproba %>%
-  pivot_longer(cols = diffusion:permutation, names_to = "type", values_to = "proba") %>%
-  ggplot(mapping = aes(x = proba, group = type, color = type)) +
-  geom_density() +
-  theme_bw() +
-  xlab("probability") +
-  labs(caption = "J. Gravier, 2022", 
-       subtitle = "Diffusion sequence from 0.01 to 0.99 every 0.05")
-
-ggsave(filename = "proba_2nd_model.png", plot = last_plot(), width = 15, height = 10, units = 'cm')
-
 # data output
 jumpdiffmodel <- read.csv(file = "outputs_data/o_t_jumpdiffmodel_size1000_t10_p11_v2.csv") %>%
   as_tibble()
@@ -216,12 +382,12 @@ jumpdiffmodel <- read.csv(file = "outputs_data/o_t_jumpdiffmodel_size1000_t10_p1
 # Ot
 jumpdiffmodel %>%
   mutate(Ot = Nt/N0,N=1000) %>%
-  ggplot(aes(x = time_t, y = Ot, color = N0/N, group = N0/N)) +
+  ggplot(aes(x = time_t/10, y = Ot, color = N0/N, group = N0/N)) +
   scale_color_viridis_c(option = "C") +
   geom_line() +
   theme_bw() +
-  scale_x_continuous(name = TeX(r"($t$)"), breaks = seq(1,10,1)) +
-  scale_y_continuous(name = TeX(r"($O_{t}$)"), breaks = seq(1,10,1)) +
+  scale_x_continuous(name = TeX(r"($t/T$)")) +
+  scale_y_continuous(name = TeX(r"($O_{t}$)")) +
   labs(caption = "J. Gravier, 2022", 
        title = "Rank turnover", subtitle = "N=1000, t=10, facetting is P(P)") +
   facet_wrap(~round(proba,3))
@@ -248,8 +414,39 @@ jumpdiffmodel %>%
 ggsave(filename = "rank_turnover_rate_nornalized_2nd_models.png", 
        plot = last_plot(), width = 18, height = 12, units = 'cm')
 
+### Ft
+jumpdiffmodelft <- read.csv(file = "outputs_data/f_t_jumpdiffmodel_size1000_t10_p11_v2.csv") %>%
+  as_tibble()
 
+jumpdiffmodelft %>%
+  filter(N0 %in% seq(1,1000,20)) %>%
+  mutate(`N0/N` = N0/1000) %>%
+  ggplot(aes(x = time_t1/10, y = Ft/1000, group = `N0/N`, color = `N0/N`)) +
+  scale_color_viridis_c(option = "C") +
+  geom_line() +
+  theme_bw() +
+  scale_x_continuous(name = TeX(r"($t/T$)")) +
+  scale_y_continuous(name = TeX(r"($F_{t}$)"), breaks = seq(0,1,0.25), limits = c(0,1)) +
+  labs(caption = "J. Gravier, 2022", 
+       title = "Rank flux", subtitle = "N=1000, t=10, facetting is P(P)") +
+  facet_wrap(~round(proba,3))
 
+ggsave(filename = "rank_flux_2nd_models.png", plot = last_plot(), width = 25, height = 18, units = 'cm')
 
+# rqnk flux mean
+jumpdiffmodelft <- read.csv(file = "outputs_data/Flux_mean_t_jumpdiffmodel_size1000_t10_p11_v2.csv") %>%
+  as_tibble()
 
+jumpdiffmodelft %>%
+  ggplot(aes(x = N0/1000, y = Ft_proba, color = proba, group = factor(proba))) +
+  scale_color_viridis_c() +
+  geom_line() +
+  theme_bw() +
+  scale_x_continuous(name = TeX(r"($N_{0}/N$)")) +
+  ylab(TeX(r"($F$)")) +
+  labs(caption = "J. Gravier, 2022", 
+       title = "Mean rank flux rate, N=1000, t=10")
+
+ggsave(filename = "rank_flux_rate_nornalized_2nd_models.png", 
+       plot = last_plot(), width = 18, height = 12, units = 'cm')
 
